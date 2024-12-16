aaf0392f2754ba75bda51c567608a7cef7bd3e4a
umair-jami <lalaumair92@gmail.com>
2024-12-16 13:00:28 +0500

    updated code

--- clerk-auth/app/layout.tsx
+++ clerk-auth/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+    <ClerkProvider publishableKey="pk_test_bWFzc2l2ZS1wdWctNTMuY2xlcmsuYWNjb3VudHMuZGV2JA">
       <html lang="en">
         <body>
           {children}
